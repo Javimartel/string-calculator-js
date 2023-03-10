b36313cdde227c351f1382be43b8f7402ece7567
Javi <javiermespino@gmail.com>
2023-03-10 14:24:24 +0000

    Test 5

--- stringCalculator.js
+++ stringCalculator.js
@@ -2,7 +2,7 @@ function stringCalculator(string) {
     if (string === '') {
         return "0";
     }
-    let array = string.split(',');
+    let array = string.split(/[,\n]/);
     return array.reduce((a, b) => parseInt(a) + parseInt(b)).toString();
 }
 
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,9 +1,5 @@
 const stringCalculator = require('./stringCalculator');
 
-// "" => "0"
-// "1" => "1"
-// "1,2" => "3"
-
 test('devuelve 0 si la string esta vacia', () => {
     expect(stringCalculator('')).toBe("0");
     }
@@ -19,4 +15,12 @@ test('devuelve 3 si la string es 1,2', () => {
 
 test('devuelve 6 si la string es 1,2,3', () => {
     expect(stringCalculator('1,2,3')).toBe("6");
+});
+
+test('devuelve 6 si la string es 1\n2,3', () => {
+    expect(stringCalculator('1\n2,3')).toBe("6");
+});
+
+test('devuelve 6 si la string es 1,2\n3', () => {
+    expect(stringCalculator('1,2\n3')).toBe("6");
 });
