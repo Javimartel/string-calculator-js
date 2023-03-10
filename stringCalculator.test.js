050095929ae2b8181e803d9885c4e33b5066b755
Javi <javiermespino@gmail.com>
2023-03-10 14:18:33 +0000

    Test 4

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -16,3 +16,7 @@ test('devuelve 1 si la string es 1', () => {
 test('devuelve 3 si la string es 1,2', () => {
     expect(stringCalculator('1,2')).toBe("3");
 });
+
+test('devuelve 6 si la string es 1,2,3', () => {
+    expect(stringCalculator('1,2,3')).toBe("6");
+});
